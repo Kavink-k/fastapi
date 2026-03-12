6feaa05f97d5c3506dd084dd58063068ab8eac9a
Kavink-k <kavikavin847@gmail.com>
2026-03-12 15:39:54 +0530

    env update

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv 
+import os;
 
-
-db_url = "postgresql://kavin:kingkavin000@localhost:5432/kavin"
+load_dotenv()
+db_url = os.getenv("db_url")
 
 engine =   create_engine(db_url)
 session = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
-
-
